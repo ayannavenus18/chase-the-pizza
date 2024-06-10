ce29952de368ba3fc57a458e69954ab6bb982c64
ayannavenus18 <ayannavenus18@gmail.com>
2024-06-10 17:49:11 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    pizza.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+    info.startCountdown(32)
+})
+let pizza: Sprite = null
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 f . f d f 
+    . f 2 2 2 2 2 2 b b f f d f 
+    . f b d d d d d d b b d b f 
+    . f d d d d d b d d f f f . 
+    . f d f f f d f f d f . . . 
+    . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+pizza = sprites.create(img`
+    ............3333bb..bb33333.....
+    ........3bb31111d3b311d177d33...
+    .......3bdd11111dbd11d17112113..
+    .......bdddd1111bd11d222dd22113.
+    ......3d111dd111b11d1222d32211d3
+    ......3d11111dd1d11d111d11d33113
+    ....bb3d111111dd13dd111d1dd3b31b
+    ...b3d3dd111111dd13dd11d1dddbbdb
+    ...3ddd31d111111dd133dddddddb.b.
+    ..311111d1ddd1111dd11dddddd33...
+    ..3111111d111dd111dd1111dd3313..
+    ..bddd1111ddd11dd111d111111113..
+    ..311ddd111dddd11dd11ddd1111ddb.
+    ..31111dd111dddd11dd111dddddddb.
+    ...bd1111d1113ddd11dd1111111d3b.
+    ...4dd1111d1113ddd11ddd111d333b.
+    ..4dbdddd11d11133ddddddddddddb..
+    .4ddbddddd11d111d33ddddddddd3b..
+    .4dddb11ddd11dd111d333dddd3bb...
+    .4dd55b111d11dd11111d3333bbb....
+    .445555b111d11dddd111111ddb.....
+    .4455555bd1d311ddddddddddd3.....
+    .45455555bb1d3111ddddddd113.....
+    .4554555555b333d1111111113......
+    455554555555bbb33d11111d33......
+    4d555545555555dbbb3d11d33.......
+    4dd5555455555ddddd43333.........
+    45dd555544ddddddd4..............
+    .45dd5555d44dddd4...............
+    ..45dd55dddd4444................
+    ...45dd55444....................
+    ....44444.......................
+    `, SpriteKind.Food)
